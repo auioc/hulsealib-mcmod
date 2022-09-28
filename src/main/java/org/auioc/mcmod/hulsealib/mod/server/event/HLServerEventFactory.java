728a5eac365f2871b6c03ec1596aa0f2cdb4c8e9
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-28 22:12:27 +0800

    feat(next): ServerPlayerSendMessageEvent

--- src/main/java/org/auioc/mcmod/hulsealib/mod/server/event/HLServerEventFactory.java
+++ src/main/java/org/auioc/mcmod/hulsealib/mod/server/event/HLServerEventFactory.java
@@ -1,12 +1,16 @@
 package org.auioc.mcmod.hulsealib.mod.server.event;
 
 import java.util.Random;
+import java.util.UUID;
 import javax.annotation.Nullable;
 import org.auioc.mcmod.arnicalib.game.entity.MobStance;
 import org.auioc.mcmod.hulsealib.game.event.server.CatMorningGiftChanceEvent;
 import org.auioc.mcmod.hulsealib.game.event.server.FishingRodCastEvent;
 import org.auioc.mcmod.hulsealib.game.event.server.ItemHurtEvent;
 import org.auioc.mcmod.hulsealib.game.event.server.PiglinStanceEvent;
+import org.auioc.mcmod.hulsealib.game.event.server.ServerPlayerSendMessageEvent;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
@@ -20,6 +24,10 @@ import net.minecraftforge.eventbus.api.IEventBus;
 public final class HLServerEventFactory {
 
     private static final IEventBus BUS = MinecraftForge.EVENT_BUS;
+
+    public static boolean onServerPlayerSendMessage(ServerPlayer player, Component message, ChatType type, UUID uuid) {
+        return BUS.post(new ServerPlayerSendMessageEvent(player, message, type, uuid));
+    }
 
     public static double onCatSetMorningGiftChance(Cat cat, Player ownerPlayer) {
         var event = new CatMorningGiftChanceEvent(cat, ownerPlayer);
