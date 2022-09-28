6d44291f69666b2190b3e3d2ad95606fd0762d69
WakelessSloth56 <wsloth56@foxmail.com>
2022-09-28 22:57:32 +0800

    feat(next): ClientboundDrawParticleShapePacket

--- src/main/java/org/auioc/mcmod/hulsealib/mod/common/network/HLPacketHandler.java
+++ src/main/java/org/auioc/mcmod/hulsealib/mod/common/network/HLPacketHandler.java
@@ -1,14 +1,14 @@
 package org.auioc.mcmod.hulsealib.mod.common.network;
 
-import org.auioc.mcmod.arnicalib.ArnicaLib;
 import org.auioc.mcmod.arnicalib.game.network.HPacketHandler;
 import org.auioc.mcmod.arnicalib.game.network.IHPacket;
+import org.auioc.mcmod.hulsealib.HulseaLib;
 import net.minecraft.server.level.ServerPlayer;
 
 public final class HLPacketHandler {
 
     private static final String PROTOCOL_VERSION = Integer.toString(1);
-    protected static final HPacketHandler HANDLER = new HPacketHandler(ArnicaLib.id("main"), PROTOCOL_VERSION);
+    protected static final HPacketHandler HANDLER = new HPacketHandler(HulseaLib.id("main"), PROTOCOL_VERSION);
 
     public static void init() {
         HLPackets.register();
--- src/main/java/org/auioc/mcmod/hulsealib/mod/common/network/HLPackets.java
+++ src/main/java/org/auioc/mcmod/hulsealib/mod/common/network/HLPackets.java
@@ -1,9 +1,12 @@
 package org.auioc.mcmod.hulsealib.mod.common.network;
 
+import static org.auioc.mcmod.hulsealib.mod.common.network.HLPacketHandler.HANDLER;
+import org.auioc.mcmod.hulsealib.mod.common.network.packet.client.ClientboundDrawParticleShapePacket;
+
 public final class HLPackets {
 
     protected static void register() {
-        // HANDLER.registerServerToClient(ClientboundDrawParticleShapePacket.class, ClientboundDrawParticleShapePacket::decode);
+        HANDLER.registerServerToClient(ClientboundDrawParticleShapePacket.class, ClientboundDrawParticleShapePacket::decode);
     }
 
 }
