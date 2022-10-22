8d54946ccd2f2a7a694563c48106d4da1ebe05f3
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-22 09:53:09 +0800

    feat(game.network): ClientboundSetExhaustionPacket

--- src/main/java/org/auioc/mcmod/hulsealib/mod/common/network/HLPackets.java
+++ src/main/java/org/auioc/mcmod/hulsealib/mod/common/network/HLPackets.java
@@ -2,11 +2,13 @@ package org.auioc.mcmod.hulsealib.mod.common.network;
 
 import static org.auioc.mcmod.hulsealib.mod.common.network.HLPacketHandler.HANDLER;
 import org.auioc.mcmod.hulsealib.mod.common.network.packet.client.ClientboundDrawParticleShapePacket;
+import org.auioc.mcmod.hulsealib.mod.common.network.packet.client.ClientboundSetExhaustionPacket;
 
 public final class HLPackets {
 
     protected static void register() {
         HANDLER.registerServerToClient(ClientboundDrawParticleShapePacket.class, ClientboundDrawParticleShapePacket::decode);
+        HANDLER.registerServerToClient(ClientboundSetExhaustionPacket.class, ClientboundSetExhaustionPacket::decode);
     }
 
 }
---
+++ src/main/java/org/auioc/mcmod/hulsealib/mod/common/network/packet/client/ClientboundSetExhaustionPacket.java
@@ -0,0 +1,34 @@
+package org.auioc.mcmod.hulsealib.mod.common.network.packet.client;
+
+import org.auioc.mcmod.arnicalib.game.network.IHPacket;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.network.NetworkEvent.Context;
+
+public class ClientboundSetExhaustionPacket implements IHPacket {
+
+    private final float exhaustion;
+
+    public ClientboundSetExhaustionPacket(float exhaustion) {
+        this.exhaustion = exhaustion;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    @SuppressWarnings("resource")
+    public void handle(Context ctx) {
+        Minecraft.getInstance().player.getFoodData().setExhaustion(this.exhaustion);
+    }
+
+    @Override
+    public void encode(FriendlyByteBuf buffer) {
+        buffer.writeFloat(this.exhaustion);
+    }
+
+    public static ClientboundSetExhaustionPacket decode(FriendlyByteBuf buffer) {
+        return new ClientboundSetExhaustionPacket(buffer.readFloat());
+    }
+
+}
