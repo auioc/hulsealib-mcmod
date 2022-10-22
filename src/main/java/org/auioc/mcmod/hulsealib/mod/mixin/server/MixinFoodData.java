e8f9a9f09c6082a9b25b09809699794b1137132f
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-22 10:24:07 +0800

    fix(game.mixin): fix setLastExhaustion

--- src/main/java/org/auioc/mcmod/hulsealib/mod/mixin/server/MixinFoodData.java
+++ src/main/java/org/auioc/mcmod/hulsealib/mod/mixin/server/MixinFoodData.java
@@ -2,6 +2,7 @@ package org.auioc.mcmod.hulsealib.mod.mixin.server;
 
 import org.auioc.mcmod.hulsealib.mod.common.network.HLPacketHandler;
 import org.auioc.mcmod.hulsealib.mod.common.network.packet.client.ClientboundSetExhaustionPacket;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,7 +26,7 @@ public class MixinFoodData {
         require = 1,
         allow = 1
     )
-    public void tick_head(Player p_38711_, CallbackInfo ci) {
+    public void tick_sendExhaustion(Player p_38711_, CallbackInfo ci) {
         if (this.lastExhaustionLevel != this.exhaustionLevel) {
             HLPacketHandler.sendToClient((ServerPlayer) p_38711_, new ClientboundSetExhaustionPacket(this.exhaustionLevel));
         }
@@ -33,11 +34,16 @@ public class MixinFoodData {
 
     @Inject(
         method = "Lnet/minecraft/world/food/FoodData;tick(Lnet/minecraft/world/entity/player/Player;)V",
-        at = @At(value = "TAIL"),
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/world/entity/player/Player;level:Lnet/minecraft/world/level/Level;",
+            opcode = Opcodes.GETFIELD,
+            ordinal = 1
+        ),
         require = 1,
         allow = 1
     )
-    public void tick_tail(Player p_38711_, CallbackInfo ci) {
+    public void tick_setLastExhaustion(Player p_38711_, CallbackInfo ci) {
         this.lastExhaustionLevel = this.exhaustionLevel;
     }
 
