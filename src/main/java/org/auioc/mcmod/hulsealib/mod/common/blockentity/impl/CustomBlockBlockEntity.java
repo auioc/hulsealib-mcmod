8a3791b6de89189869f889fd7e87158533c99aed
WakelessSloth56 <wsloth56@foxmail.com>
2023-01-17 23:04:22 +0800

    refactor(mod.block): custom block tile stores translation as vector3f

--- src/main/java/org/auioc/mcmod/hulsealib/mod/common/blockentity/impl/CustomBlockBlockEntity.java
+++ src/main/java/org/auioc/mcmod/hulsealib/mod/common/blockentity/impl/CustomBlockBlockEntity.java
@@ -1,6 +1,5 @@
 package org.auioc.mcmod.hulsealib.mod.common.blockentity.impl;
 
-import java.util.Optional;
 import javax.annotation.Nullable;
 import org.auioc.mcmod.arnicalib.game.nbt.NbtUtils;
 import org.auioc.mcmod.hulsealib.mod.common.blockentity.HLBlockEntities;
@@ -16,7 +15,6 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class CustomBlockBlockEntity extends BlockEntity {
@@ -24,7 +22,7 @@ public class CustomBlockBlockEntity extends BlockEntity {
     public static final String DEFAULT_MODEL_ID = "hulsealib:custom_block#";
     private static final VoxelShape DEFAULT_SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
     private static final double[] DEFAULT_RAW_SHAPE = new double[] {0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D};
-    private static final Vec3 DEFAULT_TRANSLATION = new Vec3(0.0D, 0.0D, 0.0D);
+    private static final Vector3f DEFAULT_TRANSLATION = new Vector3f(0.0F, 0.0F, 0.0F);
     private static final Quaternion DEFAULT_ROTATION = new Quaternion(0.0F, 0.0F, 0.0F, true);
     private static final Vector3f DEFAULT_RAW_ROTATION = new Vector3f(0.0F, 0.0F, 0.0F);
     private static final Vector3f DEFAULT_SCALE = new Vector3f(1.0F, 1.0F, 1.0F);
@@ -36,13 +34,13 @@ public class CustomBlockBlockEntity extends BlockEntity {
     @Nullable
     private VoxelShape shape;
     @Nullable
-    private Vec3 translation;
+    private Vector3f scale;
+    @Nullable
+    private Vector3f translation;
     @Nullable
     private Vector3f rawRotation;
     @Nullable
     private Quaternion rotation;
-    @Nullable
-    private Vector3f scale;
     private int light = 0;
 
     public CustomBlockBlockEntity(BlockPos pos, BlockState state) {
@@ -51,37 +49,26 @@ public class CustomBlockBlockEntity extends BlockEntity {
 
     public String getModelId() { return (modelId != null) ? modelId : DEFAULT_MODEL_ID; }
 
-    public VoxelShape getShape() {
-        return (shape != null) ? shape : DEFAULT_SHAPE;
-    }
+    public VoxelShape getShape() { return (shape != null) ? shape : DEFAULT_SHAPE; }
 
-    public Vec3 getTranslation() {
-        return (translation != null) ? translation : DEFAULT_TRANSLATION;
-    }
+    public double[] getRawShape() { return (rawShape != null) ? rawShape : DEFAULT_RAW_SHAPE; }
 
-    public Quaternion getRotation() {
-        return (rotation != null) ? rotation : DEFAULT_ROTATION;
-    }
+    public Vector3f getScale() { return (scale != null) ? scale : DEFAULT_SCALE; }
 
-    public Vector3f getScale() {
-        return (scale != null) ? scale : DEFAULT_SCALE;
-    }
+    public Vector3f getTranslation() { return (translation != null) ? translation : DEFAULT_TRANSLATION; }
 
-    public int getLight() {
-        return light;
-    }
+    public Quaternion getRotation() { return (rotation != null) ? rotation : DEFAULT_ROTATION; }
+
+    public Vector3f getRawRotation() { return (rawRotation != null) ? rawRotation : DEFAULT_RAW_ROTATION; }
+
+    public int getLight() { return light; }
 
     // ====================================================================== //
 
     @Override
     protected void saveAdditional(CompoundTag nbt) {
         super.saveAdditional(nbt);
-        if (modelId != null) nbt.putString("Model", modelId.toString());
-        if (rawShape != null && rawShape.length == 6) nbt.put("Shape", NbtUtils.writeDoubleArray(rawShape));
-        if (translation != null) nbt.put("Translation", NbtUtils.writeVec3(translation));
-        if (rawRotation != null) nbt.put("Rotation", NbtUtils.writeVector3f(rawRotation));
-        if (scale != null) nbt.put("Scale", NbtUtils.writeVector3f(scale));
-        nbt.putInt("Light", light);
+        saveData(nbt, modelId, rawShape, scale, translation, rawRotation, light);
     }
 
     @Override
@@ -96,24 +83,30 @@ public class CustomBlockBlockEntity extends BlockEntity {
             this.rawShape = DEFAULT_RAW_SHAPE;
             this.shape = DEFAULT_SHAPE;
         }
-        this.translation = NbtUtils.getVec3OrElse(nbt, "Translation", DEFAULT_TRANSLATION);
+        this.scale = NbtUtils.getVector3fOrElse(nbt, "Scale", DEFAULT_SCALE);
+        this.translation = NbtUtils.getVector3fOrElse(nbt, "Translation", DEFAULT_TRANSLATION);
         var rawRotation = NbtUtils.getVector3fOrElse(nbt, "Rotation", DEFAULT_RAW_ROTATION);
         this.rawRotation = rawRotation;
         this.rotation = new Quaternion(rawRotation.x(), rawRotation.y(), rawRotation.z(), true);
-        this.scale = NbtUtils.getVector3fOrElse(nbt, "Scale", DEFAULT_SCALE);
         this.light = Mth.clamp(nbt.getInt("Light"), 0, 15);
+    }
+
+    public static CompoundTag saveData(CompoundTag nbt, @Nullable String modelId, @Nullable double[] rawShape, @Nullable Vector3f scale, @Nullable Vector3f translation, @Nullable Vector3f rawRotation, int light) {
+        if (modelId != null) nbt.putString("Model", modelId.toString());
+        if (rawShape != null && rawShape.length == 6) nbt.put("Shape", NbtUtils.writeDoubleArray(rawShape));
+        if (scale != null) nbt.put("Scale", NbtUtils.writeVector3f(scale));
+        if (translation != null) nbt.put("Translation", NbtUtils.writeVector3f(translation));
+        if (rawRotation != null) nbt.put("Rotation", NbtUtils.writeVector3f(rawRotation));
+        nbt.putInt("Light", light);
+        return nbt;
     }
 
     // ====================================================================== //
 
     @Override
-    public Packet<ClientGamePacketListener> getUpdatePacket() {
-        return ClientboundBlockEntityDataPacket.create(this);
-    }
+    public Packet<ClientGamePacketListener> getUpdatePacket() { return ClientboundBlockEntityDataPacket.create(this); }
 
     @Override
-    public CompoundTag getUpdateTag() {
-        return this.saveWithoutMetadata();
-    }
+    public CompoundTag getUpdateTag() { return this.saveWithoutMetadata(); }
 
 }
