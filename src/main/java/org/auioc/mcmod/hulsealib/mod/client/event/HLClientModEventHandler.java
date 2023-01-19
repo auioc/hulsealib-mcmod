17a9966e81957271dd4f6e1b13dd81a09d6cd366
WakelessSloth56 <wsloth56@foxmail.com>
2023-01-19 19:45:20 +0800

    feat(game.entity): custom entity renderer

--- src/main/java/org/auioc/mcmod/hulsealib/mod/client/event/HLClientModEventHandler.java
+++ src/main/java/org/auioc/mcmod/hulsealib/mod/client/event/HLClientModEventHandler.java
@@ -2,17 +2,12 @@ package org.auioc.mcmod.hulsealib.mod.client.event;
 
 import org.auioc.mcmod.hulsealib.mod.client.model.CustomBlockBakedModel;
 import org.auioc.mcmod.hulsealib.mod.client.renderer.CustomBlockBlockEntityRenderer;
-import org.auioc.mcmod.hulsealib.mod.client.resource.CustomBlockModelReloadListener;
-import net.minecraft.client.resources.model.ModelBakery;
-import net.minecraft.server.packs.resources.ResourceManager;
+import org.auioc.mcmod.hulsealib.mod.client.renderer.CustomEntityRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityRenderersEvent.RegisterRenderers;
 import net.minecraftforge.client.event.ModelBakeEvent;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ForgeModelBakery;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
 @OnlyIn(Dist.CLIENT)
 public final class HLClientModEventHandler {
@@ -25,6 +20,7 @@ public final class HLClientModEventHandler {
     @SubscribeEvent
     public static void onRendererRegister(RegisterRenderers event) {
         CustomBlockBlockEntityRenderer.register(event);
+        CustomEntityRenderer.register(event);
     }
 
 }
